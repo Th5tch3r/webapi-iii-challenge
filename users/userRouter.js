a1d69fe8dbcf48a06229909d314dc887f2fcd7dc
Th5tch3r <amber.phan27@gmail.com>
2019-11-07 16:42:12 -0500

    finishing with router.post

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,13 +1,32 @@
 const express = 'express';
 
 const router = express.Router();
+const Users = require('./userDb');
+const Post = require('../posts/postDb');
 
+//Add a user to db
+//Requires a name
 router.post('/', (req, res) => {
-
+    Users.insert(req.body)
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({message: "User needs a name"})
+        })
 });
 
+//Add a post to a user's account 
 router.post('/:id/posts', (req, res) => {
-
+    Post.insert(req.body)
+        .then( post => {
+            res.status(201).json(post);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({message: "Error adding post"})
+        })
 });
 
 router.get('/', (req, res) => {
